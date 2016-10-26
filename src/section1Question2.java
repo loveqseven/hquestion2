6cf338ae86028a22ccb73599e7f7c6ae850160b9
loveqseven <007loveq7@gmail.com>
2016-10-26 22:02:26 +0100

    section 1 - question 1 section 1 - question 2

--- src/section1Question2.java
+++ src/section1Question2.java
@@ -1,29 +1,22 @@
 /**
  * Question 2: Whats wrong with the given code below:
- public class Main {
- public static void main(String[] args) {
- System.out.println(fixSpelling("hostleworld"));
- }
- public static String fixSpelling(String name) {
- String wordToCheck = new String(name);
- if(wordToCheck == "hostleworld" ) {
- name = "hostelworld";
- } else {
- fixSpelling(name);
- }
- return name;
- }
- }
- *
  */
 public class section1Question2 {
     public static void main(String[] args) {
         System.out.println(fixSpelling("hostleworld"));
     }
+
     public static String fixSpelling(String name) {
-        if(name == "hostleworld" ) {
+        //String wordToCheck = new String(name);
+        // TODO:This line created a different object, should should return false when using ==　because they are different objectｓ
+        String wordToCheck = name;
+        if (wordToCheck == "hostleworld") {
             name = "hostelworld";
+        } else {
+            fixSpelling(name);
         }
         return name;
     }
 }
+
+
